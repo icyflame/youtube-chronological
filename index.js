7cbf9197debf6a357a2cda9fb3ea4bed3372522f
Truong Viet Trung <vitruong@expedia.com>
2017-10-07 14:26:11 +0800

    Make playlist a module to run in main script

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const async = require('async');
 const Promise = require('bluebird');
 const _ = require('lodash');
 
+const playlist = require('./playlist');
+
 // BASE URLS
 const CHANNELS_BASE = "https://www.googleapis.com/youtube/v3/channels";
 const ACTIVITIES_BASE = "https://www.googleapis.com/youtube/v3/activities";
 const SEARCH_BASE = "https://www.googleapis.com/youtube/v3/search";
 const PLAYLIST_ITEMS_BASE = "https://www.googleapis.com/youtube/v3/playlistItems";
 
-let basePlaylistId = process.env.BASE_PLAYLIST_ID;
 let channels = process.env.CHANNELS.split(',');
 let updatePlaylist = process.env.UPDATE_PLAYLIST && process.env.UPDATE_PLAYLIST === "true";
 
@@ -32,132 +33,139 @@ Promise.map(channels, username => {
   return rp(options);
 })
 .then(channels => {
-  // 2: Get video IDs for these channels
-
-  console.log(channels);
-  let channelIds = _.map(channels, channel => {
-    if (channel.items.length > 0) {
-      return channel.items[0].id;
-    }
-  });
-
-  console.log("Ids: ", channelIds);
-
-  channelIds = [ channelIds[0] ];
-
-  let channelIdsCopy = _.clone(channelIds);
-
-  let nextPageTokens = { };
-  let allVideos = { };
-
-  async.whilst(
-    () => !_.isEmpty(channelIds),
-    (callback) => {
-      Promise.map(channelIds, (id, index) => {
-        let options = {
-          uri: SEARCH_BASE,
-          qs: {
-            channelId: id,
-            part: "snippet",
-            type: "video",
-            order: "date",
-            maxResults: 50,
-            pageToken: nextPageTokens[id]
-          },
-          headers: {
-            Authorization: "Bearer " + process.env.YOUTUBE_OAUTH2_TOKEN
-          },
-          json: true
-        };
-
-        return rp(options);
-      }).then(videoLists => {
-        nextPageTokens = { };
-
-        _.map(channelIds, (id, index) => {
-          if (_.isUndefined(allVideos[id])) {
-            allVideos[id] = [ ];
-          }
-
-          let uploadedItems = videoLists[index].items;
-
-          allVideos[id] = _.concat(allVideos[id] || [ ], uploadedItems);
-
-          let thisChannelsNextPageToken = videoLists[index].nextPageToken;
-
-          if (_.isUndefined(thisChannelsNextPageToken)) {
-            _.pull(channelIds, id);
-          } else {
-            nextPageTokens[id] = thisChannelsNextPageToken;
-          }
-        });
-
-        callback(null);
-      })
-      .catch(error => {
-        console.log("Error while retrieving the video IDs");
-        console.error(error);
-      });
-    }, error => {
-      if (error) {
-        console.log("PROBLEMS!");
-        console.error(error);
-      } else {
-        _.each(allVideos, (value, key) => {
-          console.log(`${key} has ${value.length} videos`);
-          _.reverse(value);
-          console.log(require('util').inspect(value[0], { depth: null }));
-
-          value = _.filter(value, video => video.id && video.id.kind === 'youtube#video');
-
-          if (!updatePlaylist) {
-            console.log();
-            console.log();
-            console.log("Update playlist boolean is set to false in this script");
-            console.log("To make changes to the playlist, open the index.js file and change the value of updatePlaylist to true");
-            process.exit(0);
-          }
-
-          console.log();
-          console.log("Will add " + value.length + " videos to the given playlist.");
-          console.log("This might take some time, please wait patiently!");
-          console.log();
-
-          Promise.each(value, (video, index) => {
-            let playlistItemRes = {
-              kind: "youtube#playlistItem",
-              snippet: {
-                playlistId: basePlaylistId,
-                resourceId: video.id
-              }
-            };
-
-            let options = {
-              method: "POST",
-              uri: PLAYLIST_ITEMS_BASE,
-              qs: {
-                part: "snippet"
-              },
-              body: playlistItemRes,
-              headers: {
-                Authorization: "Bearer " + process.env.YOUTUBE_OAUTH2_TOKEN
-              },
-              json: true
-            };
-
-            return rp(options);
-          }).then(response => {
-            console.log();
-            console.log();
-            console.log("PLAYLIST UPDATED!");
-            console.log("Check this page: https://youtube.com/playlist?list=" + basePlaylistId);
-          }).catch(error => {
-            console.log("Problems while adding videos to playlist!");
-            console.error(error);
-          });
-        });
+  playlist(process.env.CHANNELS)
+  .then((playlistId) => {
+    //assign base playlist id
+    let basePlaylistId = playlistId;
+    // 2: Get video IDs for these channels
+    console.log(channels);
+    let channelIds = _.map(channels, channel => {
+      if (channel.items.length > 0) {
+        return channel.items[0].id;
       }
     });
+
+    console.log("Ids: ", channelIds);
+
+    channelIds = [ channelIds[0] ];
+
+    let channelIdsCopy = _.clone(channelIds);
+
+    let nextPageTokens = { };
+    let allVideos = { };
+
+    async.whilst(
+      () => !_.isEmpty(channelIds),
+      (callback) => {
+        Promise.map(channelIds, (id, index) => {
+          let options = {
+            uri: SEARCH_BASE,
+            qs: {
+              channelId: id,
+              part: "snippet",
+              type: "video",
+              order: "date",
+              maxResults: 50,
+              pageToken: nextPageTokens[id]
+            },
+            headers: {
+              Authorization: "Bearer " + process.env.YOUTUBE_OAUTH2_TOKEN
+            },
+            json: true
+          };
+
+          return rp(options);
+        }).then(videoLists => {
+          nextPageTokens = { };
+
+          _.map(channelIds, (id, index) => {
+            if (_.isUndefined(allVideos[id])) {
+              allVideos[id] = [ ];
+            }
+
+            let uploadedItems = videoLists[index].items;
+
+            allVideos[id] = _.concat(allVideos[id] || [ ], uploadedItems);
+
+            let thisChannelsNextPageToken = videoLists[index].nextPageToken;
+
+            if (_.isUndefined(thisChannelsNextPageToken)) {
+              _.pull(channelIds, id);
+            } else {
+              nextPageTokens[id] = thisChannelsNextPageToken;
+            }
+          });
+
+          callback(null);
+        })
+        .catch(error => {
+          console.log("Error while retrieving the video IDs");
+          console.error(error);
+        });
+      }, error => {
+        if (error) {
+          console.log("PROBLEMS!");
+          console.error(error);
+        } else {
+          _.each(allVideos, (value, key) => {
+            console.log(`${key} has ${value.length} videos`);
+            _.reverse(value);
+            console.log(require('util').inspect(value[0], { depth: null }));
+
+            value = _.filter(value, video => video.id && video.id.kind === 'youtube#video');
+
+            if (!updatePlaylist) {
+              console.log();
+              console.log();
+              console.log("Update playlist boolean is set to false in this script");
+              console.log("To make changes to the playlist, open the index.js file and change the value of updatePlaylist to true");
+              process.exit(0);
+            }
+
+            console.log();
+            console.log("Will add " + value.length + " videos to the given playlist.");
+            console.log("This might take some time, please wait patiently!");
+            console.log();
+
+            Promise.each(value, (video, index) => {
+              let playlistItemRes = {
+                kind: "youtube#playlistItem",
+                snippet: {
+                  playlistId: basePlaylistId,
+                  resourceId: video.id
+                }
+              };
+
+              let options = {
+                method: "POST",
+                uri: PLAYLIST_ITEMS_BASE,
+                qs: {
+                  part: "snippet"
+                },
+                body: playlistItemRes,
+                headers: {
+                  Authorization: "Bearer " + process.env.YOUTUBE_OAUTH2_TOKEN
+                },
+                json: true
+              };
+
+              return rp(options);
+            }).then(response => {
+              console.log();
+              console.log();
+              console.log("PLAYLIST UPDATED!");
+              console.log("Check this page: https://youtube.com/playlist?list=" + basePlaylistId);
+            }).catch(error => {
+              console.log("Problems while adding videos to playlist!");
+              console.error(error);
+            });
+          });
+        }
+      });
+  },
+  err => {
+    console.log(err);
+  });
 })
 .catch(error => {
   console.log("Error while retrieving the channel IDs");
--- playlist.js
+++ playlist.js
@@ -3,42 +3,58 @@ require('dotenv').config();
 
 // Load dependencies
 const rp = require('request-promise');
-const fs = require('fs');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
 
 // Define constants
 const PLAYLISTS_API_ENDPOINT = 'https://www.googleapis.com/youtube/v3/playlists';
+const ENCODING = 'utf-8';
+const ENV_FILE = '.env';
+
+// Util method to update .env file
+var updateEnvFile = function(playlistId) {
+  return new Promise(function(resolve, reject) {
+    fs.readFileAsync(ENV_FILE, ENCODING)
+    .then(data => {
+      const newData = data.replace(/BASE_PLAYLIST_ID=.*\n/, 'BASE_PLAYLIST_ID=' + playlistId + '\n');
+      return fs.writeFileAsync(ENV_FILE, newData, ENCODING);
+    }, reject)
+    .then(() => {
+      console.log('\nNEW PLAYLIST ID: ' + playlistId + '\n');
+      console.log('ENV Updated!\n');
+      resolve(playlistId);
+    }, reject);
+  });
+}
 
 // Call youtube data api to create a playlist
-const options = {
-  method: 'POST',
-  uri: PLAYLISTS_API_ENDPOINT,
-  qs: {
-    part: 'snippet,status',
-  },
-  body: {
-    snippet: {
-      title: 'youtube-chronological ' + Date().toString()
+var playlist = function(channelName) {
+  const options = {
+    method: 'POST',
+    uri: PLAYLISTS_API_ENDPOINT,
+    qs: {
+      part: 'snippet,status',
     },
-    status: {
-      privacyStatus: 'private'
-    }
-  },
-  headers: {
-    Authorization: 'Bearer ' + process.env.YOUTUBE_OAUTH2_TOKEN
-  },
-  json: true
-};
-rp(options).then(res => {
-  const playlistId = res.id;
+    body: {
+      snippet: {
+        title: channelName + ' Chronological ' + (new Date()).toISOString().replace(/T.*$/,'')
+      },
+      status: {
+        privacyStatus: 'private'
+      }
+    },
+    headers: {
+      Authorization: 'Bearer ' + process.env.YOUTUBE_OAUTH2_TOKEN
+    },
+    json: true
+  };
+  return new Promise(function(resolve, reject) {
+    rp(options).then(res => {
+      const playlistId = res.id;
+      updateEnvFile(playlistId)
+      .then(resolve, reject);
+    }, reject);
+  });
+}
 
-  // Read and update .env file
-  const envData = fs.readFileSync('.env', 'utf-8');
-  // This will always replace BASE_PLAYLIST_ID with the new value, more logic would be needed if
-  // we want to check for existing playlist
-  const newData = envData.replace(/BASE_PLAYLIST_ID=.*\n/, 'BASE_PLAYLIST_ID=' + playlistId + '\n');
-  fs.writeFileSync('.env', newData, 'utf-8');
-  console.log('\nNEW PLAYLIST ID: ' + playlistId + '\n');
-  console.log('ENV Updated!\n');
-}, err => {
-  console.log(err);
-});
+module.exports = playlist;
